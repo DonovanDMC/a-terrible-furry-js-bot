af460a5b79a0b0b6a89d7904427342b2bb047d01
Skullbite <mawler12@gmail.com>
2018-10-08 09:21:29 -0400

    Create botto.js

---
+++ botto.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log("Can i go back to bed now??");
+}); 
+
+bot.login(big_gay_token)
