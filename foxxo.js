ec050b6e4ccbdf67cadf110f347cbb3b775558aa
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-24 23:07:16 -0600

    this will fully work

--- foxxo.js
+++ foxxo.js
@@ -1,18 +1,50 @@
 const Discord = require('discord.js');
-const fox = new Discord.Client();
-const { prefix, token } = require('./config.json'); // aka fakeconfig.json :v
+const config = require('./config.json'); // aka fakeconfig.json :v
 
-fox.on('ready', () => {
-    console.log("Can i go back to bed now??");
-}); 
-
-fox.on('message', msg => {
-    if (msg.content === `${prefix}ping`) {
-        msg.channel.send(`N-nice of you to check on me ${msg.author.username}, eheh..`);
+class Fox extends Discord.Client {
+   constructor(ClientOptions) {
+       super(ClientOptions);
+       this.Discord = Discord;
+       this.config = config;
+       
+       this.on("ready", this._ready.bind(this));
+       this.on("message", this._message.bind(this));
+       this.load.apply(this);
+   }
+    
+    async load() {
+        console.log("Loading Client things..");
+        // nothing yet :v
     }
-    else if (msg.content === `${prefix}userinfo`) {
-        return;
+    
+    async _ready() {
+        console.log(`Can I go back to bed now? I'm ready with ${this.guilds.size} guilds..`);
     }
-});
+    
+    async _message(message) {
+        if(!message || message.author.bot) return;
+        if(message.channel.type === "dm") return message.channel.send("H-hey.. Wh-what are you doing direct messaging me? >w<");
+        var prefix = message.content.startsWith(`<@${this.user.id}>`)?`<@${this.user.id}>`:message.content.startsWith(`<@!${this.user.id}>`)?`<@!${this.user.id}>`:this.config.prefix,
+		args = message.content.slice(prefix.length).trim().split(/\s+/g),
+		command = args.shift().toLowerCase();
+        
+        if(!message.content.startsWith(prefix)) return;
+        
+        switch(command) {
+                case "ping":
+                    message.channel.startTyping();
+                    var m = await message.channel.send("Checking Ping..");
+                    m.edit("Ping Calculated!");
+                    m.delete().catch(noerr=>{});
+                    message.channel.send(`N-nice of you to check on me, ${message.author.username}...\nBot Ping: ${(m.createdTimestamp - message.createdTimestamp)}ms${"\n"}API Ping: ${Math.round(this.ws.ping)}ms`);
+                    return message.channel.stopTyping();
+                    break;
 
-fox.login(token);
+                default:
+                    return;
+        }
+    }
+}
+const fox = new Fox({disableEveryone:true});
+
+fox.login(config.token);
